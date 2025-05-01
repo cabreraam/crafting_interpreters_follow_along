e8681687e307097062aaeda5b78eb10f3ccf99c1
Anthony Cabrera <cabreraam@ieee.org>
2025-05-01 08:18:38 -0500

    implemented the rest of chapter 7
    that includes ch 7.3 and 7.4

--- jlox_interpreter/src/main/java/com/craftinginterpreters/lox/Interpreter.java
+++ jlox_interpreter/src/main/java/com/craftinginterpreters/lox/Interpreter.java
@@ -1,6 +1,15 @@
 package com.craftinginterpreters.lox;
 
 class Interpreter implements Expr.Visitor<Object> {
+  void interpret(Expr expression) {
+    try {
+      Object value = evaluate(expression);
+      System.out.println(stringify(value));
+    } catch (RuntimeError error) {
+      Lox.runtimeError(error);
+    }
+  }
+
   @Override
   public Object visitLiteralExpr(Expr.Literal expr) {
     return expr.value;
@@ -14,11 +23,25 @@ class Interpreter implements Expr.Visitor<Object> {
     case BANG:
       return !isTruthy(right);
     case MINUS:
+      checkNumberOperand(expr.operator, right);
       return -(double)right;
     }
 
     // Unreachable.
     return null;
+  }
+
+  private void checkNumberOperand(Token operator, Object operand) {
+    if (operand instanceof Double)
+      return;
+    throw new RuntimeError(operator, "Operand must be a number.");
+  }
+
+  private void checkNumberOperands(Token operator, Object left, Object right) {
+    if (left instanceof Double && right instanceof Double)
+      return;
+
+    throw new RuntimeError(operator, "Operands must be numbers.");
   }
 
   @Override
@@ -45,6 +68,22 @@ class Interpreter implements Expr.Visitor<Object> {
     return a.equals(b);
   }
 
+  private String stringify(Object object) {
+    if (object == null)
+      return "nil";
+
+    if (object instanceof Double) {
+      String text = object.toString();
+      // just removes the ".0" if it exists
+      if (text.endsWith(".0")) {
+        text = text.substring(0, text.length() - 2);
+      }
+      return text;
+    }
+
+    return object.toString();
+  }
+
   @Override
   public Object visitBinaryExpr(Expr.Binary expr) {
     Object left = evaluate(expr.left);
@@ -52,14 +91,19 @@ class Interpreter implements Expr.Visitor<Object> {
 
     switch (expr.operator.type) {
     case GREATER:
+      checkNumberOperands(expr.operator, left, right);
       return (double)left > (double)right;
     case GREATER_EQUAL:
+      checkNumberOperands(expr.operator, left, right);
       return (double)left >= (double)right;
     case LESS:
+      checkNumberOperands(expr.operator, left, right);
       return (double)left < (double)right;
     case LESS_EQUAL:
+      checkNumberOperands(expr.operator, left, right);
       return (double)left <= (double)right;
     case MINUS:
+      checkNumberOperands(expr.operator, left, right);
       return (double)left - (double)right;
     case PLUS:
       if (left instanceof Double && right instanceof Double) {
@@ -70,10 +114,14 @@ class Interpreter implements Expr.Visitor<Object> {
         return (String)left + (String)right;
       }
 
-      break;
+      throw new RuntimeError(expr.operator,
+                             "Operands must be two numbers or two strings.");
+
     case SLASH:
+      checkNumberOperands(expr.operator, left, right);
       return (double)left / (double)right;
     case STAR:
+      checkNumberOperands(expr.operator, left, right);
       return (double)left * (double)right;
     case BANG_EQUAL:
       return !isEqual(left, right);
