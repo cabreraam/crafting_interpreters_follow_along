adb5cce6c7b17f1543f9f3bb48e5e8aff6588f85
Anthony Cabrera <cabreraam@ieee.org>
2025-05-03 10:49:57 -0500

    add parsing routines for Expr.Print and Expr.Statement

--- jlox_interpreter/src/main/java/com/craftinginterpreters/lox/Parser.java
+++ jlox_interpreter/src/main/java/com/craftinginterpreters/lox/Parser.java
@@ -2,6 +2,7 @@ package com.craftinginterpreters.lox;
 
 import static com.craftinginterpreters.lox.TokenType.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 class Parser {
@@ -12,16 +13,42 @@ class Parser {
 
   Parser(List<Token> tokens) { this.tokens = tokens; }
 
-  Expr parse() {
-    try {
-      return expression();
-    } catch (ParseError error) {
-      return null;
+  List<Stmt> parse() {
+    // try {
+    // return expression();
+    // } catch (ParseError error) {
+    // return null;
+    // }
+    List<Stmt> statements = new ArrayList<>();
+    while (!isAtEnd()) {
+      statements.add(statement());
     }
+
+    return statements;
   }
 
   // expression → equality ;
   private Expr expression() { return equality(); }
+
+  private Stmt statement() {
+    if (match(PRINT)) {
+      return printStatement();
+    }
+
+    return expressionStatement();
+  }
+
+  private Stmt printStatement() {
+    Expr value = expression();
+    consume(SEMICOLON, "Expect ';' after value.");
+    return new Stmt.Print(value);
+  }
+
+  private Stmt expressionStatement() {
+    Expr expr = expression();
+    consume(SEMICOLON, "Expect ';' after value.");
+    return new Stmt.Expression(expr);
+  }
 
   // equality → comparison ( ( "!=" | "==" ) comparison )* ;
   private Expr equality() {
