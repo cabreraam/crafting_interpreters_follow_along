4e40d575d63e94f5322d8a0df1ce39566d7a97c1
Anthony Cabrera <cabreraam@ieee.org>
2025-06-21 11:05:31 -0500

    adding block scoping for Environment.java as detailed in 8.5.2
    Signed-off-by: Anthony Cabrera <cabreraam@ieee.org>

--- jlox_interpreter/src/main/java/com/craftinginterpreters/lox/Environment.java
+++ jlox_interpreter/src/main/java/com/craftinginterpreters/lox/Environment.java
@@ -4,12 +4,25 @@ import java.util.HashMap;
 import java.util.Map;
 
 class Environment {
+  // variable that tracks what environment precedes us in scope
+  final Environment enclosing;
+  // note that `values` gets a default definition outside of the constructor
   private final Map<String, Object> values = new HashMap();
+
+  // global scope environment constructor
+  Environment() { enclosing = null; }
+
+  // nested scope environment constructor
+  Environment(Environment enclosing) { this.enclosing = enclosing; }
 
   Object get(Token name) {
     if (values.containsKey(name)) {
       return values.get(name.lexeme);
     }
+
+    // recursively search for variable if not found in current scope
+    if (enclosing != null)
+      return enclosing.get(name);
 
     throw new RuntimeError(name, "Undefined variable '" + name.lexeme + "'.");
   }
@@ -17,6 +30,12 @@ class Environment {
   void assign(Token name, Object value) {
     if (values.containsKey(name.lexeme)) {
       values.put(name.lexeme, value);
+      return;
+    }
+
+    // recursively search for variable if not found in current scope
+    if (enclosing != null) {
+      enclosing.assign(name, value);
       return;
     }
 
