a191bbcb00ed9de74cf2fe0605c2d4fc7b674cde
Anthony Cabrera <cabreraam@ieee.org>
2025-09-09 21:26:27 -0500

    10.2: add a native function that reports time between two invocations
    Signed-off-by: Anthony Cabrera <cabreraam@ieee.org>

--- jlox_interpreter/src/main/java/com/craftinginterpreters/lox/Interpreter.java
+++ jlox_interpreter/src/main/java/com/craftinginterpreters/lox/Interpreter.java
@@ -10,7 +10,32 @@ import java.util.List;
 
 class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
 
-  private Environment environment = new Environment();
+  // private Environment environment = new Environment();
+  final Environment globals = new Environment();
+  private Environment environment = globals;
+
+  Interpreter() {
+    // in this constructor, we
+    // create anonymous class that implements LoxCallable x-face.
+    // note that before this commit, there was no explicit constructor
+    // defined.
+    globals.define("clock", new LoxCallable() {
+      @Override
+      public int arity() {
+        return 0;
+      }
+
+      @Override
+      public Object call(Interpreter interpreter, List<Object> arguments) {
+        return (double)System.currentTimeMillis() / 1000.0;
+      }
+
+      @Override
+      public String toString() {
+        return "<native fn>";
+      }
+    });
+  }
 
   void interpret(List<Stmt> statements) {
     try {
