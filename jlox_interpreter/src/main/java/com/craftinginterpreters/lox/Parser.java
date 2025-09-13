4163155e0f87dde57d29f076cec9d4c006d845f2
Anthony Cabrera <cabreraam@ieee.org>
2025-09-13 15:26:28 -0500

    left out a line to consume left paren when creating a function AST node
    Signed-off-by: Anthony Cabrera <cabreraam@ieee.org>

--- jlox_interpreter/src/main/java/com/craftinginterpreters/lox/Parser.java
+++ jlox_interpreter/src/main/java/com/craftinginterpreters/lox/Parser.java
@@ -180,6 +180,7 @@ class Parser {
   private Stmt.Function function(String kind) {
     Token name = consume(IDENTIFIER, "Expect " + kind + " name.");
 
+    consume(LEFT_PAREN, "Expect '(' after " + kind + " name.");
     List<Token> parameters = new ArrayList<>();
     if (!(check(RIGHT_PAREN))) {
       do {
